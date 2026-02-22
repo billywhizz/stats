9ff9858623c05eb39f225a25bda7f32ceb51ef67
Andrew Johnston <apjohnsto@gmail.com>
2026-02-22 23:42:54 +0000

    chore: rename dir

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-mkdir -p dist
-npx uglify-js -o dist/simple.min.js --compress --mangle -- simple.js
-npx uglify-js -o dist/sqlite.min.js --compress --mangle -- sqlite.js
-cp sqlite.wasm dist/
-cp simple.html dist/index.html
-mkdir -p dist/db
-cp db/*.db dist/db/
-cp android-icon-192x192.png dist/
-cp site.webmanifest dist/
+DEST=docs
+mkdir -p $DEST
+npx uglify-js -o $DEST/simple.min.js --compress --mangle -- simple.js
+npx uglify-js -o $DEST/sqlite.min.js --compress --mangle -- sqlite.js
+cp sqlite.wasm $DEST/
+cp simple.html $DEST/index.html
+mkdir -p $DEST/db
+cp db/*.db $DEST/db/
+cp android-icon-192x192.png $DEST/
+cp site.webmanifest $DEST/
